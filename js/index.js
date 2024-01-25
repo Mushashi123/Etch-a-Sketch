4ebe961e53eff1f513ed7e1a825d0b3f0d8bdcdf
Sovit <tsovit729@gmail.com>
2024-01-25 21:19:53 +0545

    seperated event handler function definition, and refactored the codes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
+// || Constants
 const MIN_GRID_SIZE = 1;
 const MAX_GRID_SIZE = 100;
 const MODE_PEN = "pen";
 const MODE_ERASER = "eraser";
 const MODE_RANDOMIZE = "randomize";
+
+// || DOM element selection for DOM manipulation
 const canvas = document.querySelector(".canvas");
 const currentStatus = document.querySelector(".status__current");
 const resizeBtn = document.querySelector("#resize");
@@ -11,79 +14,86 @@ const pickColorBtn = document.querySelector("#pick-color");
 const colorPicker = document.querySelector("#color-picker");
 const randomizeColorBtn = document.querySelector("#randomize-color");
 const resetBtn = document.querySelector("#reset");
-//pen works only if mouse is down
+
+// || Main logic
+
+// variables that stores states, change in this varibles causes effect in behaviour of the application
 let penMode = MODE_PEN;
 let mouseDown = false;
 let penColor = "#000000";
 
-canvas.addEventListener("mousedown", (e) => {
+canvas.addEventListener("mousedown", mouseIsDown);
+
+// if mouse button is released, pen won't work
+canvas.addEventListener("mouseup", mouseReleased);
+
+//if the mouse leaves the canvas while mouse button is down, it should be considered mouse releaased, so that when mouse returns to the canvas, user must press mousebutton down again to use pen functionality
+// mouseleave doesn't bubble up and descendent element doesn't fire the leave event , hence we can detect if mouse is out of canvas itself or it's descendant
+canvas.addEventListener("mouseleave", mouseReleased);
+
+// pen works only if mouse button is down and enters over a pixel's body
+canvas.addEventListener("mouseover", pen);
+
+//resize feature
+resizeBtn.addEventListener("click", resizeGird);
+
+//eraser feature, toogles erasery
+eraserBtn.addEventListener("click", eraser);
+
+//randomize color feature, random pixels
+randomizeColorBtn.addEventListener("click", randomizeColor);
+
+//change event occours if user changes color  in the color picker and dismisses the color  picker
+colorPicker.addEventListener("change", changePenColor);
+
+resetBtn.addEventListener("click", resetPixels);
+
+// initial grid creation (16 * 16)
+createGrid(undefined);
+
+// || function declarations
+// no function expressions or arrow functions are used cause they are not hoisted
+
+function mouseIsDown(e) {
   mouseDown = true;
+
   // if target node it not an element node do nothing
-  if (!(e.target.nodeType === Node.ELEMENT_NODE)) {
+  if (e.target.nodeType !== Node.ELEMENT_NODE) {
     return;
   }
-  //if mouse down happens in one of the pixel, the pixel must change color too
+
+  //if mouse down happens in one of the pixel, the pixel must get effected based on pen's mode
   if (penMode === MODE_PEN) e.target.style.backgroundColor = penColor;
-  // if pen mode is eraser, it erases the color
-  else if (penMode === MODE_ERASER) {
+  else if (penMode === MODE_ERASER)
     e.target.style.backgroundColor = "transparent";
-  } else if (penMode === MODE_RANDOMIZE) {
+  else if (penMode === MODE_RANDOMIZE)
     e.target.style.backgroundColor = randomColor();
-  }
-});
+}
 
-// if mouse if up it wont work
-canvas.addEventListener("mouseup", (e) => {
+function mouseReleased(e) {
   mouseDown = false;
-});
+}
 
-//if the mouse leaves the canvas while mouse down, it should be considered mouse out, so that when mouse returns to the canvas, user must press mouse down again to use pen
-// mouseleave doesn't bubble and descendent element doesnt fire the event , hence we can detect if mouse is out of canvas itself or it's descendant
-canvas.addEventListener("mouseleave", (e) => {
-  mouseDown = false;
-});
-
-// pen works only if mouse is down and is over a pixel
-canvas.addEventListener("mouseover", (e) => {
-  // is mouse is not down return
+function pen(e) {
+  // if mouse button is not pressed, pen won't work
   if (!mouseDown) {
     return;
   }
 
   // if target is not element node return
-  if (!(e.target.nodeType === Node.ELEMENT_NODE)) {
+  if (e.target.nodeType !== Node.ELEMENT_NODE) {
     return;
   }
 
-  if (penMode === MODE_PEN) {
-    e.target.style.backgroundColor = penColor;
-  } else if (penMode === MODE_ERASER) {
+  //based upon pen mode, alter the pixels
+  if (penMode === MODE_PEN) e.target.style.backgroundColor = penColor;
+  else if (penMode === MODE_ERASER)
     e.target.style.backgroundColor = "transparent";
-  } else if (penMode === MODE_RANDOMIZE) {
+  else if (penMode === MODE_RANDOMIZE)
     e.target.style.backgroundColor = randomColor();
-  }
-});
+}
 
-//resize feature
-resizeBtn.addEventListener("click", (e) => {
-  let newGridSize = prompt("Enter new grid size: ");
-  // if number is NaN, or nullish dont do anything
-  if (isNaN(newGridSize) || !newGridSize) {
-    return;
-  }
-
-  // if number is out of range return
-  if (newGridSize < MIN_GRID_SIZE || newGridSize > MAX_GRID_SIZE) {
-    alert(`Grid size ${newGridSize} out of range`);
-    return;
-  }
-
-  //create new grid with new size
-  createGrid(newGridSize);
-});
-
-//eraser feature, toogles erasery
-eraserBtn.addEventListener("click", (e) => {
+function eraser(e) {
   if (penMode === MODE_ERASER) {
     eraserBtn.classList.remove("btn--active");
     penMode = MODE_PEN;
@@ -93,12 +103,11 @@ eraserBtn.addEventListener("click", (e) => {
   eraserBtn.classList.add("btn--active");
   penMode = MODE_ERASER;
 
-  // remove other modes
+  // remove other active buttons
   randomizeColorBtn.classList.remove("btn--active");
-});
+}
 
-//randomize color feature, random pixels
-randomizeColorBtn.addEventListener("click", (e) => {
+function randomizeColor(e) {
   if (penMode === MODE_RANDOMIZE) {
     randomizeColorBtn.classList.remove("btn--active");
     penMode = MODE_PEN;
@@ -108,34 +117,53 @@ randomizeColorBtn.addEventListener("click", (e) => {
   randomizeColorBtn.classList.add("btn--active");
   penMode = MODE_RANDOMIZE;
 
-  //remove other mode
+  //remove other active buttons
   eraserBtn.classList.remove("btn--active");
-});
+}
 
-//change event occours if user changes color  in the color picker and dismisses the color  picker
-colorPicker.addEventListener("change", (e) => {
+function changePenColor(e) {
   let colorPicked = e.target.value;
   penColor = colorPicked;
 
   //change the current selected color's value in UI (#picked-color) element
   pickColorBtn.querySelector("#picked-color").style.backgroundColor =
     colorPicked;
-});
 
-resetBtn.addEventListener("click", (e) => {
+  //when user changes color, it means user wants to use pen , so remove any active modes and change mode to pen
+  eraserBtn.classList.remove("btn--active");
+  randomizeColorBtn.classList.remove("btn--active");
+  penMode = MODE_PEN;
+}
+
+function resetPixels(e) {
   // get the first child of canvas
   const firstPixel = canvas.firstChild;
 
-  // itterate the until the next sibbling elementh is null and erase color
+  // itterate each pixel and erase color, until next sibbling element is null
   let currentPixel = firstPixel;
   while (currentPixel) {
     currentPixel.style.backgroundColor = "transparent";
     currentPixel = currentPixel.nextElementSibling;
   }
-});
+}
 
-// initial grid default 16 * 16
-createGrid(undefined);
+function resizeGird(e) {
+  let newGridSize = prompt("Enter new grid size: ");
+
+  // if newGridSize is NaN, or nullish value don't do anything
+  if (isNaN(newGridSize) || !newGridSize) {
+    return;
+  }
+
+  // if number is out of range return error and don't do anything
+  if (newGridSize < MIN_GRID_SIZE || newGridSize > MAX_GRID_SIZE) {
+    alert(`Grid size ${newGridSize} out of range`);
+    return;
+  }
+
+  //create new grid with new size, if everything fine
+  createGrid(newGridSize);
+}
 
 function createGrid(size = 16) {
   //before creating grid make sure canvas is empty
@@ -160,6 +188,9 @@ function createGrid(size = 16) {
 }
 
 function randomColor() {
-  let randomColor = Math.trunc(Math.random() * 1234567890);
-  return `#${randomColor.toString(16).substring(0, 6)}`;
+  let red = Math.trunc(Math.random() * 256);
+  let green = Math.trunc(Math.random() * 256);
+  let blue = Math.trunc(Math.random() * 256);
+
+  return `rgb(${red},${green},${blue})`;
 }
